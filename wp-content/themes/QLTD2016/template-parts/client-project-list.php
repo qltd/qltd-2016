5434cfbb93ab41d0c89a66ae96a1f4d787fd6c34
Matt Restorff <inperpetuum@gmail.com>
2016-05-31 14:55:44 -0400

    Fixed image array link

--- wp-content/themes/QLTD2016/template-parts/client-project-list.php
+++ wp-content/themes/QLTD2016/template-parts/client-project-list.php
@@ -4,7 +4,8 @@
         <?php foreach ($clients as $client): ?>
             <li>
                 <a href="<?php echo get_term_link($client); ?>">
-                    <div class="img-container" style="background: url(<?php echo get_field('featured_image', $client->taxonomy . '_' . $client->term_id)['url']; ?>) no-repeat center center; background-size: cover;"></div>
+                    <?php $img = get_field('featured_image', $client->taxonomy . '_' . $client->term_id); ?>
+                    <div class="img-container" style="background: url(<?php echo $img['url']; ?>) no-repeat center center; background-size: cover;"></div>
                     <h3><?php echo $client->name; ?> ></h3>
                 </a>
             </li>
