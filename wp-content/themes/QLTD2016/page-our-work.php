59d2fa4e1db6a65d56e1aba5fd676624d132a9cb
Ramsey <rdarling42@gmail.com>
2016-08-31 12:51:41 -0400

    displaying client instead of project name

--- wp-content/themes/QLTD2016/page-our-work.php
+++ wp-content/themes/QLTD2016/page-our-work.php
@@ -17,16 +17,18 @@ get_template_part('template-parts/content-page'); ?>
         'order'    => 'ASC'
     );
     $projects = get_posts( $args );
-    echo '<pre>'; print_r(count($projects)); echo '</pre>';
+ //   echo '<pre>'; print_r(count($projects)); echo '</pre>';
 ?>
 <div class="project-grid">
     <?php foreach ($projects as $p):?>
+    <?php $cat = get_the_terms($p,'clients');?>
+    <?php $catname = !empty($cat[0]->name) ? $cat[0]->name : $p->post_title;?>
         <a href="<?php echo get_permalink($p->ID); ?>" class="box">
             <?php $img = get_field('featured_image', $p->ID); ?>
             <img src="<?php echo $img['sizes']['square_thumb']; ?>">
             <div class="details">
                 <div>
-                    <h2><?php echo $p->post_title; ?></h2>
+                    <h2><?php echo $catname; ?></h2>
                     <h3>
                     <?php $services = get_field('services', $p->ID);
                         echo formatServices($services);
