b5df79ebd29b3e56186229bb74a71cb8cb6d8186
Ramsey <rdarling42@gmail.com>
2016-08-31 14:14:38 -0400

    adding project name to display under client

--- wp-content/themes/QLTD2016/page-our-work.php
+++ wp-content/themes/QLTD2016/page-our-work.php
@@ -22,18 +22,14 @@ get_template_part('template-parts/content-page'); ?>
 <div class="project-grid">
     <?php foreach ($projects as $p):?>
     <?php $cat = get_the_terms($p,'clients');?>
-    <?php $catname = !empty($cat[0]->name) ? $cat[0]->name : $p->post_title;?>
+    <?php $catname = !empty($cat[0]->name) ? $cat[0]->name : '';?>
         <a href="<?php echo get_permalink($p->ID); ?>" class="box">
             <?php $img = get_field('featured_image', $p->ID); ?>
             <img src="<?php echo $img['sizes']['square_thumb']; ?>">
             <div class="details">
                 <div>
                     <h2><?php echo $catname; ?></h2>
-                    <h3>
-                    <?php $services = get_field('services', $p->ID);
-                        echo formatServices($services);
-                    ?>
-                    </h3>
+                    <h3><?php echo $p->post_title; ?></h3>
                 </div>
             </div>
         </a>
